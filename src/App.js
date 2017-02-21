005bed4ebdf4eee6b78f310a7ccc728b3b37c8bc
Stas-Buzunko <buzunkostas@gmail.com>
2017-02-21 17:29:33 +0700

    add firebase login

--- src/App.js
+++ src/App.js
@@ -1,11 +1,60 @@
 import React, { Component } from 'react';
+import firebase from 'firebase'
+import SignUp from './components/signUp'
+import toastr from 'toastr'
+import 'toastr/build/toastr.min.css'
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    const authenticatedFromStorage = localStorage.getItem('top-notch-productivity')
+    const authenticated = authenticatedFromStorage !== null
+
+    this.state = {
+      authenticated,
+      id: ''
+    }
+
+    this.signUpWithGoogle = this.signUpWithGoogle.bind(this)
+  }
+
+  componentDidMount() {
+    firebase.auth().onAuthStateChanged(user => {
+      if (user) {
+        console.log('logged in');
+        this.setState({authenticated: true, id: user.uid});
+        localStorage.setItem('top-notch-productivity', 'true');
+      } 
+      else {
+        this.setState({authenticated: false, id: ''});
+        localStorage.removeItem('top-notch-productivity', 'true');
+        console.log('not logged in');
+      }
+    });
+  }
+
+  signUpWithGoogle() {
+    const { authenticated } = this.state
+    if (!authenticated) {
+      const provider = new firebase.auth.GoogleAuthProvider();
+      firebase.auth().signInWithPopup(provider)
+      .then(() => {
+        toastr.success('You are in!')
+      })
+      .catch(error => {
+        toastr.error(error.message)
+      });
+    }
+  }
+
   render() {
+    const { authenticated } = this.state;
+    console.log(authenticated)
     return (
     
         <div className="routerView">
+        {!authenticated && <SignUp signUpWithGoogle={this.signUpWithGoogle} />}
         {this.props.children}
 
       </div>
--- src/components/signUp.js
+++ src/components/signUp.js
@@ -3,22 +3,15 @@ import { Link, browserHistory } from 'react-router';
 import './components.css';
 
 class SignUp extends Component {
- onSignIn(googleUser) {
-var profile = googleUser.getBasicProfile();
-console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
-console.log('Name: ' + profile.getName());
-console.log('Image URL: ' + profile.getImageUrl());
-console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
-};
-
   pushToActivity() {
     browserHistory.push({pathname: '/addActivity'})
   }
+
   render () {
     return (
       <div className="outer"  style={{ height:'300px'}}>
         <span className="inner">
-          <div className="g-signin2" data-onsuccess="onSignIn" onClick={() => this.pushToActivity()}></div>
+          <button onClick={this.props.signUpWithGoogle}>Google</button>
         </span>
       </div>
     );
