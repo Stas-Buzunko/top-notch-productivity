c20301c8781d918450727ca17fef628d76492249
Xirisss <shchasnovich.daniil@gmail.com>
2017-05-10 13:43:08 +0300

    Add settingsPage, add navbar

---
+++ src/pages/SettingsPage.js
@@ -0,0 +1,82 @@
+import React, { Component } from 'react'
+import firebase from 'firebase'
+import { FormControl, Button } from 'react-bootstrap'
+
+class Settings extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      togglKey: '',
+      workspace: '',
+      currentRate: '',
+      preference: false
+    }
+  }
+
+  componentDidMount() {
+    const userId = firebase.auth().currentUser.uid;
+    firebase.database().ref('/users/' + userId + '/settings').once('value').then(snapshot => {
+      this.setState({
+        togglKey: snapshot.val().togglKey,
+        workspace: snapshot.val().workspace,
+        currentRate: snapshot.val().currentRate,
+        preference: snapshot.val().preference
+      })
+    });
+  }
+
+  handleInputChange(field, value) {
+    this.setState({
+      [field]: value,
+    })
+    console.log(this.state)
+  }
+
+  numberChecker(value) {
+    if (!isNaN(value)) {
+      this.setState({
+        currentRate: value,
+      })
+      console.log(this.state)
+    }
+  }
+
+  handleSubmit() {
+    const { togglKey, workspace, currentRate, preference } = this.state
+    if (togglKey.length && workspace.length && currentRate.length) {
+      this.setState({error: ''});
+      this.settingsCustomer({togglKey, workspace, currentRate, preference});
+    } else {
+      this.setState({error: 'Please enter correct details'});
+    }
+  }
+
+  settingsCustomer(params) {
+    const { togglKey, workspace, currentRate, preference } = params
+    const userUid = this.props.user.uid
+    firebase.database().ref('users/' + userUid + '/settings').update({
+      togglKey,
+      workspace,
+      currentRate,
+      preference
+    })   
+  }
+
+  render() {
+    const { togglKey, workspace, currentRate, preference } = this.state;
+    return (
+      <div>
+        <FormControl value={togglKey} type="text" placeholder="Toogl API key" onChange={e => this.handleInputChange('togglKey', e.target.value)} /><br />
+        <FormControl value={workspace} type="text" placeholder="Workspace" onChange={e => this.handleInputChange('workspace', e.target.value)} /><br />
+        <FormControl value={currentRate} type="text" placeholder="Current Rate" onChange={e => this.numberChecker(e.target.value)} /><br />
+        <FormControl componentClass="select" value={preference} onChange={e => this.handleInputChange('preference', e.target.value)}>
+          <option value={true}>24h</option>
+          <option value={false}>till midnight</option>
+        </FormControl><br />
+        <Button onClick={() => this.handleSubmit()}>Save settings</Button>
+      </div>
+    )
+  }
+}
+
+export default Settings;
--- src/routes.js
+++ src/routes.js
@@ -5,11 +5,13 @@ import App from './App';
 import Payment from './pages/PaymentPage';
 import ActivitiesPage from './pages/ActivitiesPage';
 import MainPage from './pages/MainPage';
+import Settings from './pages/SettingsPage';
 
 export default (
   <Route path="/" component={App}>
     <IndexRoute component={MainPage} />
     <Route path="activities" component={ActivitiesPage} />
     <Route path="payment" component={Payment} />
+    <Route path="settings" component={Settings} />
   </Route>
 );
