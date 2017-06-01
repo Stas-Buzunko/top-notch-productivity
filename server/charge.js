2f50b2914f5d2064234770fe33b084e52e832877
Xirisss <shchasnovich.daniil@gmail.com>
2017-06-01 18:54:02 +0300

    test

--- server/charge.js
+++ server/charge.js
@@ -3,6 +3,19 @@ var admin = require("firebase-admin");
 exports.charge = function() {
   admin.database().ref('/days').orderByChild('isDayOver').equalTo(false).once('value').then(function(snapshot) {
     var qwe = snapshot.val()
-    console.log(qwe)
+    const qwe1 = qwe.slice()
+    Object.keys(qwe1).map((key) => {
+      if (qwe1[key].how_long) {
+        const left = Number(qwe1[key].startedAt) + 25 * 60 * 60 * 1000
+      } else {
+        const left = Number(qwe1[key].startedAt) + 60 * 60 * 1000 + 
+        (24 * 60 * 60 * 1000 - Number(qwe1[key].startedAt) % (24 * 60 * 60 * 1000));
+      }
+      console.log(left)
+      if (left < Date.now) {
+        qwe1[key].isDayOver = true
+      }
+    })
+    // console.log(qwe)
   })
 }
